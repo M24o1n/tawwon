6be0828481804cb1a47274310a1aaa6634399bb2
Mohammad Almuallimi <78993584+M24o1n@users.noreply.github.com>
2023-04-10 06:26:41 +0300

    final

--- lib/screens/order_details2.dart
+++ lib/screens/order_details2.dart
@@ -8,19 +8,22 @@ class OrderDetails2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 186, 200, 218),
+        backgroundColor: const Color.fromARGB(255, 218, 229, 234),
         appBar: AppBar(
-          title: const Text('تفاصيل الطلب'),
+          title: const Text(
+            'تفاصيل الطلب',
+            style: TextStyle(fontSize: 28, fontFamily: 'ReadexPro'),
+          ),
           centerTitle: true,
           leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
+            icon: const Icon(Icons.arrow_back_ios_new),
             onPressed: () {
               Navigator.pop(context);
             },
           ),
           actions: [
             IconButton(
-              icon: const Icon(Icons.menu),
+              icon: const Icon(Icons.menu_outlined),
               onPressed: () {},
             ),
           ],
@@ -31,10 +34,10 @@ class OrderDetails2 extends StatelessWidget {
             children: [
               Container(
                 decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 167, 167, 167),
+                    color: const Color.fromARGB(220, 220, 220, 220),
                     borderRadius: BorderRadius.circular(20)),
                 child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: const EdgeInsets.fromLTRB(8.0, 12, 8.0, 40),
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
@@ -42,7 +45,14 @@ class OrderDetails2 extends StatelessWidget {
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.end,
-                            children: const [Text(':النوع')],
+                            children: const [
+                              Text(
+                                ':النوع',
+                                style: TextStyle(
+                                    fontSize: 26,
+                                    color: Color.fromARGB(255, 33, 55, 83)),
+                              )
+                            ],
                           ),
                         ),
                         Padding(
@@ -56,7 +66,14 @@ class OrderDetails2 extends StatelessWidget {
                                     borderRadius: BorderRadius.circular(20)),
                                 child: const Padding(
                                   padding: EdgeInsets.all(8.0),
-                                  child: Text('بلاستيك'),
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                                    child: Text('بلاستيك',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color.fromARGB(
+                                                255, 33, 55, 83))),
+                                  ),
                                 ),
                               ),
                             ],
@@ -66,7 +83,12 @@ class OrderDetails2 extends StatelessWidget {
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.end,
-                            children: const [Text(':الوقت')],
+                            children: const [
+                              Text(':الوقت',
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      color: Color.fromARGB(255, 33, 55, 83)))
+                            ],
                           ),
                         ),
                         Padding(
@@ -80,7 +102,14 @@ class OrderDetails2 extends StatelessWidget {
                                     borderRadius: BorderRadius.circular(20)),
                                 child: const Padding(
                                   padding: EdgeInsets.all(8.0),
-                                  child: Text('3:00PM - 9:00PM'),
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                                    child: Text('3:00PM - 9:00PM',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color.fromARGB(
+                                                255, 33, 55, 83))),
+                                  ),
                                 ),
                               ),
                             ],
@@ -90,7 +119,14 @@ class OrderDetails2 extends StatelessWidget {
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.end,
-                            children: const [Text(':المكان')],
+                            children: const [
+                              Text(
+                                ':المكان',
+                                style: TextStyle(
+                                    fontSize: 26,
+                                    color: Color.fromARGB(255, 33, 55, 83)),
+                              )
+                            ],
                           ),
                         ),
                         Padding(
@@ -104,8 +140,15 @@ class OrderDetails2 extends StatelessWidget {
                                     borderRadius: BorderRadius.circular(20)),
                                 child: const Padding(
                                   padding: EdgeInsets.all(8.0),
-                                  child:
-                                      Text('Academic Belt Road, Dhahran 31261'),
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                                    child: Text(
+                                        'Academic Belt Road, Dhahran 31261',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color.fromARGB(
+                                                255, 33, 55, 83))),
+                                  ),
                                 ),
                               ),
                             ],
@@ -115,7 +158,12 @@ class OrderDetails2 extends StatelessWidget {
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.end,
-                            children: const [Text(':المنظمة المطلوبة')],
+                            children: const [
+                              Text(':المنظمة المطلوبة',
+                                  style: TextStyle(
+                                      fontSize: 26,
+                                      color: Color.fromARGB(255, 33, 55, 83)))
+                            ],
                           ),
                         ),
                         Padding(
@@ -129,7 +177,14 @@ class OrderDetails2 extends StatelessWidget {
                                     borderRadius: BorderRadius.circular(20)),
                                 child: const Padding(
                                   padding: EdgeInsets.all(8.0),
-                                  child: Text('Nestle'),
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(10, 2, 10, 2),
+                                    child: Text('Nestle',
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color.fromARGB(
+                                                255, 33, 55, 83))),
+                                  ),
                                 ),
                               ),
                             ],
@@ -138,35 +193,50 @@ class OrderDetails2 extends StatelessWidget {
                       ]),
                 ),
               ),
-              ElevatedButton(
-                  // When pressed it will present a dialog box
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            backgroundColor:
-                                const Color.fromARGB(255, 186, 200, 218),
-                            content: const Text(
-                              'تم ارسال طلبك',
-                              textAlign: TextAlign.center,
-                            ),
-                            actions: [
-                              Center(
-                                child: TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('عودة'),
+              const SizedBox(height: 90),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                // round edges
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                contentPadding:
+                                    const EdgeInsets.symmetric(vertical: 100),
+                                backgroundColor:
+                                    const Color.fromARGB(255, 218, 229, 234),
+                                content: const Text(
+                                  'تم ارسال طلبك',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      color: Color.fromARGB(255, 33, 55, 83)),
                                 ),
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                  child: const Text(
-                    'تاكيد الطلب',
-                  ))
+                                actions: [
+                                  Center(
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: const Text('عودة',
+                                          style: TextStyle(fontSize: 16)),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            });
+                      },
+                      child: const Text('تاكيد الطلب',
+                          style: TextStyle(fontSize: 16))),
+                ),
+              )
             ],
           ),
         ));
